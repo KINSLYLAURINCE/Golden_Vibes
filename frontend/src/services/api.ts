270f1a7298295a5ec29144e4d59133425e568bfc
KINSLYLAURINCE <kingslydebruyne17@gmail.com>
2026-03-12 20:58:49 +0100

    taf kinsly artiste

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 
 
 export const API_URL =
-  import.meta.env.VITE_API_URL ?? "https://api.goldenvibes-event.com";
+  import.meta.env.VITE_API_URL ?? "https://api.goldenvibes-event.com/api";
 
 export const STORAGE_URL =
   import.meta.env.VITE_STORAGE_URL ?? "https://api.goldenvibes-event.com/storage";
